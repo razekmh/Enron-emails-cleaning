83a1cef1fa0bae6de8f946f9c4bb179c12201049
Mahmoud <razekmh@gmail.com>
2022-03-30 02:42:23 +0000

    update neo4j data transform

--- utils/enron_to_neo4j.py
+++ utils/enron_to_neo4j.py
@@ -25,91 +25,46 @@ df_transactions = pd.read_csv(enron_pg_path / 'unique_email_users.csv')
 
 # create user nodes
 with open (enron_neo4j_path / 'nodes.csv', "w") as users_file:
-    fieldnames = ["user_id","user_email","first_name","last_name","rank"]  
-    nodes_writer = csv.DictWriter( users_file, delimiter=',', quoting=csv.QUOTE_ALL, fieldnames=fieldnames)
-    nodes_writer.writerow({"user_id":"user_id:ID","user_email":"user_email","first_name":"first_name","last_name":"last_name","rank":"rank"}) 
+    fieldnames = ["user_id","user_email","first_name","last_name","rank","role","company"]  
+    nodes_writer = csv.DictWriter( users_file, delimiter=',', quoting=csv.QUOTE_ALL, fieldnames=fieldnames, restval="")
+    nodes_writer.writerow({"user_id":"user_id:ID","user_email":"user_email","first_name":"first_name","last_name":"last_name","rank":"rank","role":"role","company":"company"}) 
     nodes_writer.writerow({"user_id":"user_000000"})
     for index, row in df_users.iterrows():
-        #clean email address
-        row['user_email'] = row['user_email'].replace("'", "")
+        write_dict = {}
+        write_dict['user_email'] = row['user_email'].replace("'", "")
         #standardize user id
-        row['user_id'] = "user_" + str(row['user_id']).zfill(6)
+        write_dict['user_id'] = "user_" + str(row['user_id']).zfill(6)
         if row['first_name'] != 'None' and not pd.isna(row['first_name']):
-            # clean first name
-            row['first_name'] = row['first_name'].replace("'", "")
-        else:
-            row['first_name'] = ''
+            write_dict['first_name'] = row['first_name'].replace("'", "")
         if row['last_name'] != 'None' and not pd.isna(row['last_name']):
-            # clean last name
-            row['last_name'] = row['last_name'].replace("'", "")
-        else: 
-            row['last_name'] = ''
+            write_dict['last_name'] = row['last_name'].replace("'", "")
         if not pd.isna(row['rank']):
-            pass
-        else:
-            row['rank'] = ''
-        
-        nodes_writer.writerow(row)
+            write_dict["rank"] = row['rank']
+        if not pd.isna(row['role']) and row['role'] != "None":
+            write_dict["role"] = row["role"]
+        if not pd.isna(row["company"]) and row["company"] != "None":
+            write_dict["company"] = row["company"]
+
+        nodes_writer.writerow(write_dict)
 
 # # create email relationships
 df_relationships = pd.merge(df_transactions, df_emails, on='email_message_id', how='left')
 
-# with open (enron_neo4j_path / 'relationships.txt', 'w') as emails_file:
-#     for index, row in df_relationships.iterrows():
-#         sender_id = "user_" + str(row['sender']).zfill(6)
-#         receiver_id = "user_" + str(row['receiver']).zfill(6)
-#         emails_file.write(f"MATCH (sender:Person {{user_id:'{sender_id}'}}), (receiver:Person {{user_id:'{receiver_id}'}})")
-#         emails_file.write(f"MERGE (sender)-[:SENT_EMAIL {{date:'{row['email_date']}'")
-#         if not pd.isna(row['email_subject']):
-#             email_subject = row['email_subject'].replace("'", "")
-#             email_subject = email_subject.replace(":", " ")
-#             emails_file.write(f", subject:'{email_subject}'")
-#         emails_file.write(f", message_id:'{row['email_message_id']}'")
-#         emails_file.write(f", type: '{row['transaction_type']}'")
-#         emails_file.write(f", routing: '{row['external_or_internal']}'")
-#         emails_file.write("}]->(receiver)")
-#         emails_file.write(";\n")
-
-
-# create list of files for neo4j relationship import
-rel_counter = 0
-rel_file_stem = 'relationships'
-rel_file_name = rel_file_stem + str(rel_counter) + '.txt'
-
-for index, row in df_relationships.iterrows():
-    if rel_counter % 100000 == 0:
-        rel_file_name = rel_file_stem + str(rel_counter).zfill(9) + '.txt'
-    with open (enron_neo4j_path / rel_file_name, 'a+') as rel_file:
-        sender_id = "user_" + str(row['sender']).zfill(6)
-        receiver_id = "user_" + str(row['receiver']).zfill(6)
-        rel_file.write(f"MATCH (sender:Person {{user_id:'{sender_id}'}}), (receiver:Person {{user_id:'{receiver_id}'}})")
-        rel_file.write(f"MERGE (sender)-[:SENT_EMAIL {{date:'{row['email_date']}'")
+with open (enron_neo4j_path / 'relationships.csv', "w") as rel_file:
+    fieldnames = ["sender","email_date","email_subject","email_message_id","transaction_type","routing","receiver","rel_type"]
+    rel_writer = csv.DictWriter( rel_file, delimiter=',', quoting=csv.QUOTE_ALL, fieldnames=fieldnames, restval="")
+    rel_writer.writerow({"sender":":START_ID","email_date":"email_date:date","email_subject":"email_subject","email_message_id":"email_message_id","transaction_type":"transaction_type","routing":"routing","receiver":":END_ID","rel_type":":TYPE"})
+    for index, row in df_relationships.iterrows():
+        rel_dict = {}
+        rel_dict["sender"] = "user_" + str(row['sender']).zfill(6)
+        rel_dict["receiver"] = "user_" + str(row['receiver']).zfill(6)
+        rel_dict["email_date"] = row["email_date"]
         if not pd.isna(row['email_subject']):
             email_subject = row['email_subject'].replace("'", "")
-            email_subject = email_subject.replace(":", " ")
-            rel_file.write(f", subject:'{email_subject}'")
-        rel_file.write(f", message_id:'{row['email_message_id']}'")
-        rel_file.write(f", type: '{row['transaction_type']}'")
-        rel_file.write(f", routing: '{row['external_or_internal']}'")
-        rel_file.write("}]->(receiver)")
-        rel_file.write(";\n")
-        rel_counter += 1
-            
-
-
-#     #     emails_file.write(f"CREATE ({sender_id})-[:SENT_TO{{message_id:'{row['email_message_id']}', subject:'{row['email_subject']}', date:'{row['email_date']}', type: '{row['transaction_type']}', routing: '{row['external_or_internal']}'}}]->({receiver_id})\n")
-#     # emails_file.write(";")
-
-# rel_file.write("USING PERIODIC COMMIT 1000\n")
-#             rel_file.write("LOAD CSV WITH HEADERS FROM 'file:///relationships.txt' AS row\n")
-#             rel_file.write("CREATE (sender:Person {{user_id:row.sender}}), (receiver:Person {{user_id:row.receiver}})")
-#             rel_file.write("MERGE (sender)-[:SENT_EMAIL {{date:row.email_date")
-#             if not pd.isna(row['email_subject']):
-#                 email_subject = row['email_subject'].replace("'", "")
-#                 email_subject = email_subject.replace(":", " ")
-#                 rel_file.write(f", subject:'{email_subject}'")
-#             rel_file.write(f", message_id:'{row['email_message_id']}'")
-#             rel_file.write(f", type: '{row['transaction_type']}'")
-#             rel_file.write(f", routing: '{row['external_or_internal']}'")
-#             rel_file.write("}]->(receiver)")
-#             rel_file.write(";\n")
+            rel_dict["email_subject"] = email_subject.replace(":", " ")
+        rel_dict['email_message_id'] = row['email_message_id']
+        if row["transaction_type"] != "None":
+            rel_dict['transaction_type'] = row['transaction_type']
+        rel_dict["routing"] = row['external_or_internal']
+        rel_dict["rel_type"] = "SENT_TO"
+        rel_writer.writerow(rel_dict)    
