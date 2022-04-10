08f2425030a8d602939331448e6a4177bc63c273
Mahmoud <razekmh@gmail.com>
2022-04-10 15:23:53 +0000

    change table names sql

--- utils/postgresql_init_nouser.sql
+++ utils/postgresql_init_nouser.sql
@@ -1,19 +1,19 @@
 \connect docker docker
 
-DROP TABLE IF EXISTS email CASCADE;
+DROP TABLE IF EXISTS emails CASCADE;
 
-CREATE TABLE email (
+CREATE TABLE emails (
 		email_message_id VARCHAR (50) PRIMARY KEY,
 		email_date TIMESTAMP with time zone,
 		email_subject TEXT,
 		email_body TEXT
 		);
 
-COPY email FROM '/docker-entrypoint-initdb.d/emails.csv' DELIMITER ',' CSV HEADER;
+COPY emails FROM '/docker-entrypoint-initdb.d/emails.csv' DELIMITER ',' CSV HEADER;
 
-DROP TABLE IF EXISTS employee CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
-CREATE TABLE employee (
+CREATE TABLE users (
 		user_id VARCHAR (50) PRIMARY KEY,
 		user_email VARCHAR (250),
 		first_name VARCHAR (250),
@@ -23,28 +23,28 @@ CREATE TABLE employee (
 		company VARCHAR (250)
 		);
 
-INSERT INTO employee (user_id, user_email, first_name, last_name, rank, role, company)
+INSERT INTO users (user_id, user_email, first_name, last_name, rank, role, company)
 VALUES('0','Unknown',NULL, NULL, NULL, NULL, NULL);
 
-COPY employee FROM '/docker-entrypoint-initdb.d/unique_users_with_names.csv' DELIMITER ',' CSV HEADER;
+COPY users FROM '/docker-entrypoint-initdb.d/unique_users_with_names.csv' DELIMITER ',' CSV HEADER;
 
 
 
-DROP TABLE IF EXISTS eamil_transaction CASCADE;
+DROP TABLE IF EXISTS eamil_transactions CASCADE;
 
-CREATE TABLE eamil_transaction (
+CREATE TABLE eamil_transactions (
 		transaction_id VARCHAR (50) PRIMARY KEY,
 		email_message_id VARCHAR (50),
 		sender VARCHAR (250),
 		receiver VARCHAR (250),
 		transaction_type VARCHAR (250),
 		external_or_internal VARCHAR (250),
-		FOREIGN KEY (email_message_id) REFERENCES email(email_message_id),
-		FOREIGN KEY (sender) REFERENCES employee(user_id),
-		FOREIGN KEY (receiver) REFERENCES employee(user_id)
+		FOREIGN KEY (email_message_id) REFERENCES emails(email_message_id),
+		FOREIGN KEY (sender) REFERENCES users(user_id),
+		FOREIGN KEY (receiver) REFERENCES users(user_id)
 		);
 
-COPY eamil_transaction FROM '/docker-entrypoint-initdb.d/unique_email_users.csv' DELIMITER ',' CSV HEADER;
+COPY eamil_transactions FROM '/docker-entrypoint-initdb.d/unique_email_users.csv' DELIMITER ',' CSV HEADER;
 
 
 GRANT USAGE ON SCHEMA public TO user01;
